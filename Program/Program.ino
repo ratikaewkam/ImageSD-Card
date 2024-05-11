f046ba5fc3e4e0d13f4f4489d4da16784aacf2d6
ratikaewkam <ratikaewkam.rk@gmail.com>
2024-05-11 11:17:51 +0700

    Update SDCard.ino - Read w,h from header

--- Program/Program.ino
+++ Program/Program.ino
@@ -20,7 +20,15 @@ void setup() {
   int fileSize = readSize("IMAGE/TABLE.BMP");
   Serial.print("BMP File Size: ");
   Serial.print(fileSize);
-  Serial.println("bytes");
+  Serial.println(" bytes");
+
+  int w = readWidth("IMAGE/TABLE.BMP");
+  int h = readHeight("IMAGE/TABLE.BMP");
+  Serial.print("Image: ");
+  Serial.print(w);
+  Serial.print("x");
+  Serial.print(h);
+  Serial.println(" px");
 }
 
 void loop() {
@@ -42,6 +50,7 @@ int readSize(char* fileName) {
 
   // Read file size (4 bytes)
   uint32_t fileSize = file.read();
+  
   fileSize |= file.read() << 8;
   fileSize |= file.read() << 16;
   fileSize |= file.read() << 24;
@@ -49,4 +58,52 @@ int readSize(char* fileName) {
   file.close();
 
   return fileSize;
+}
+
+int readWidth(char* fileName) {
+  File file = SD.open(fileName, FILE_READ);
+
+  if (!file) {
+    Serial.println("Failed to open BMP file!");
+  }
+
+  /*
+   * Seek to the position where the width is stored in the BMP header
+   */
+  file.seek(18); // Start of width (4 bytes)
+
+  // Read file size (4 bytes)
+  uint32_t w = file.read();
+  
+  w |= file.read() << 8;
+  w |= file.read() << 16;
+  w |= file.read() << 24;
+
+  file.close();
+
+  return w;
+}
+
+int readHeight(char* fileName) {
+  File file = SD.open(fileName, FILE_READ);
+
+  if (!file) {
+    Serial.println("Failed to open BMP file!");
+  }
+
+  /*
+   * Seek to the position where the height is stored in the BMP header
+   */
+  file.seek(22); // Start of height (4 bytes)
+
+  // Read file size (4 bytes)
+  uint32_t h = file.read();
+  
+  h |= file.read() << 8;
+  h |= file.read() << 16;
+  h |= file.read() << 24;
+
+  file.close();
+
+  return h;
 }
