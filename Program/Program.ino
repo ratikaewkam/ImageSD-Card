a7b5978db9be0303022e01c43c78ab696a613b9b
ratikaewkam <ratikaewkam.rk@gmail.com>
2024-05-11 10:40:52 +0700

    Update Program.ino - Read file size from header

--- Program/Program.ino
+++ Program/Program.ino
@@ -1,9 +1,52 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <SPI.h>
+#include <SD.h>
 
+const int cs = 4; // Chip select pin
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // Wait for Serial Monitor to open
+  }
+
+  if (!SD.begin(cs)) {
+    Serial.println("SD card initialization failed!");
+    return;
+  }
+
+  Serial.println("SD card initialized.");
+
+  // Read BMP file size
+  int fileSize = readSize("IMAGE/TABLE.BMP");
+  Serial.print("BMP File Size: ");
+  Serial.print(fileSize);
+  Serial.println("bytes");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
+}
 
+int readSize(char* fileName) {
+  File file = SD.open(fileName, FILE_READ);
+
+  if (!file) {
+    Serial.println("Failed to open BMP file!");
+  }
+
+  /*
+   Seek to the position where the file 
+   size is stored in the BMP header
+  */
+  file.seek(2); // Start of file size (2 bytes)
+
+  // Read file size (4 bytes)
+  uint32_t fileSize = file.read();
+  fileSize |= file.read() << 8;
+  fileSize |= file.read() << 16;
+  fileSize |= file.read() << 24;
+
+  file.close();
+
+  return fileSize;
 }
